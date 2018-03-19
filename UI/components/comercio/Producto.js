795892b042b5c157d12c3b639fadc4dffcc7e256
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-03-19 15:43:02 -0500

    UI 0.1 Terminada

--- UI/components/comercio/Producto.js
+++ UI/components/comercio/Producto.js
@@ -1,31 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
 
 const styles = StyleSheet.create({
   container: {
-
+    marginTop: 8,
+    marginBottom: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   nombre: {
-    fontSize: 16,
-    textAlign: "left"
+    fontSize: 28,
+    textAlign: "left",
+    marginLeft: 10
   },
   precio: {
-    fontSize: 12,
+    fontSize: 16,
     textAlign: "right"
+  },
+  foto: {
+    height: 80,
+    width: 80
   }
 });
 
 class Producto extends React.Component {
-  render(){
+  render() {
     const { producto, seleccionar } = this.props;
-    console.log(this.props);
-    return(
+    return (
       <TouchableOpacity onPress={seleccionar}>
         <View style={styles.container}>
-          <Text style={styles.nombre}>
-            {producto.nombre}
-          </Text>
+          <View style={{flexDirection: "row", alignItems: "center"}}>
+            <Image
+              source={producto.foto}
+              style={styles.foto}
+            />
+            <Text style={styles.nombre}>
+              {producto.nombre}
+            </Text>
+          </View>
           <Text style={styles.precio}>
             {producto.precio}
           </Text>
