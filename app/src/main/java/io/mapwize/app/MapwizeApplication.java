31db661e26f0a74ee2fec12ab6a8179609ad82d2
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2018-03-19 16:02:28 -0500

    Add space for api key

--- app/src/main/java/io/mapwize/app/MapwizeApplication.java
+++ app/src/main/java/io/mapwize/app/MapwizeApplication.java
@@ -10,7 +10,10 @@ public class MapwizeApplication  extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //Default of mapwize
         AccountManager.start(this, "1f04d780dc30b774c0c10f53e3c7d4ea");
+        //My own
+
     }
 
 }
